301f31175f2f5fabde4c8b29004e27a43a63c935
Nguyenhieu277 <nguyenphamtrunghieu277@gmail.com>
2025-09-02 20:54:43 +0700

    #1 - update agent rag of admission and learning

--- app/infra/agents/sub_agents/learning_agent/agent.py
+++ app/infra/agents/sub_agents/learning_agent/agent.py
@@ -18,18 +18,19 @@ from .sub_agents.rag_agent import RAG_Agent
 
 load_dotenv()
 
-rag_agent_instance = RAG_Agent(
+learning_agent = RAG_Agent(
     name="rag_agent",
     answer_generator=LlmAgent(
         name="rag_llm",
-        model="gemini-2.5-flash"
+        model="gemini-2.5-flash",
+        instruction=learning_agent_instruction
     )
 )
 
-learning_agent = Agent(
-    name = "learning_agent",
-    description = "Tác nhân sử dụng rag_agent_instance để trả lời câu hỏi của người dùng",
-    instruction = learning_agent_instruction,
-    sub_agents = [rag_agent_instance]
-)
+# learning_agent = Agent(
+#     name = "learning_agent",
+#     description = "Tác nhân sử dụng rag_agent_instance để trả lời câu hỏi của người dùng",
+#     instruction = learning_agent_instruction,
+#     sub_agents = [rag_agent_instance]
+# )
 
